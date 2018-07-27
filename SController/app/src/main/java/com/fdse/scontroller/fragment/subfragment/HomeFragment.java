8e6283979bec3a932a9f3e8230fb289a21f65877
shenbiao <1105125966@qq.com>
2018-07-27 10:04:37 +0800

    添加listview圆角，边距和下拉刷新

--- SController/app/src/main/java/com/fdse/scontroller/fragment/subfragment/HomeFragment.java
+++ SController/app/src/main/java/com/fdse/scontroller/fragment/subfragment/HomeFragment.java
@@ -1,14 +1,17 @@
 package com.fdse.scontroller.fragment.subfragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.fdse.scontroller.R;
 import com.fdse.scontroller.adapter.HomeDeviceAdapter;
@@ -16,10 +19,13 @@ import com.fdse.scontroller.customview.HomeDevice;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class HomeFragment extends Fragment {
     private ListView listView;
+    private SwipeRefreshLayout swipeRefreshView;
     private List<HomeDevice> homeDeviceList = new ArrayList<HomeDevice>();
+    private HomeDeviceAdapter homeDeviceAdapter;
 
     public static HomeFragment newInstance(String s) {
         HomeFragment homeFragment = new HomeFragment();
@@ -33,10 +39,51 @@ public class HomeFragment extends Fragment {
 
         //获取（初始化设备数据）
         initHomeDevice();
-        HomeDeviceAdapter homeDeviceAdapter = new HomeDeviceAdapter(getActivity(),
+        homeDeviceAdapter = new HomeDeviceAdapter(getActivity(),
                 R.layout.item_home_deivce, homeDeviceList);
         listView = (ListView) view.findViewById(R.id.list_view);
         listView.setAdapter(homeDeviceAdapter);
+
+        swipeRefreshView = (SwipeRefreshLayout) view.findViewById(R.id.swipe_home_device);
+        // 设置颜色属性的时候一定要注意是引用了资源文件还是直接设置16进制的颜色，因为都是int值容易搞混
+        // 设置下拉进度的背景颜色，默认就是白色的
+        swipeRefreshView.setProgressBackgroundColorSchemeResource(android.R.color.white);
+        // 设置下拉进度的主题颜色
+        swipeRefreshView.setColorSchemeResources(R.color.colorAccent, R.color.colorPrimary, R.color.yellow);
+
+        // 下拉时触发SwipeRefreshLayout的下拉动画，动画完毕之后就会回调这个方法
+        swipeRefreshView.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                // 开始刷新，设置当前为刷新状态
+                //swipeRefreshLayout.setRefreshing(true);
+
+                // 这里是主线程
+                // 一些比较耗时的操作，比如联网获取数据，需要放到子线程去执行
+                // TODO 获取数据
+                final Random random = new Random();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        HomeDevice homeDevice1 =new HomeDevice(R.drawable.home_xiaomidfb,"小米电饭煲","暂停运行");
+                        homeDeviceList.add(homeDevice1);
+                        homeDeviceAdapter.notifyDataSetChanged();
+
+                        Toast.makeText(getActivity(), "刷新了一条数据", Toast.LENGTH_SHORT).show();
+
+                        // 加载完数据设置为不刷新状态，将下拉进度收起来
+                        swipeRefreshView.setRefreshing(false);
+                    }
+                }, 1200);
+
+                // System.out.println(Thread.currentThread().getName());
+
+                // 这个不能写在外边，不然会直接收起来
+                //swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
         return view;
     }
 
@@ -47,18 +94,6 @@ public class HomeFragment extends Fragment {
         homeDeviceList.add(homeDevice2);
         HomeDevice homeDevice3 =new HomeDevice(R.drawable.home_saodijiqiren,"扫地机器人","正在运行");
         homeDeviceList.add(homeDevice3);
-        HomeDevice homeDevice4 =new HomeDevice(R.drawable.home_saodijiqiren,"扫地机器人","正在运行");
-        homeDeviceList.add(homeDevice4);
-        HomeDevice homeDevice5 =new HomeDevice(R.drawable.home_saodijiqiren,"扫地机器人","正在运行");
-        homeDeviceList.add(homeDevice5);
-        HomeDevice homeDevice6 =new HomeDevice(R.drawable.home_saodijiqiren,"扫地机器人","正在运行");
-        homeDeviceList.add(homeDevice6);
-        HomeDevice homeDevice7 =new HomeDevice(R.drawable.home_saodijiqiren,"扫地机器人","正在运行");
-        homeDeviceList.add(homeDevice7);
-        HomeDevice homeDevice8 =new HomeDevice(R.drawable.home_saodijiqiren,"扫地机器人","正在运行");
-        homeDeviceList.add(homeDevice8);
-        HomeDevice homeDevice9 =new HomeDevice(R.drawable.home_saodijiqiren,"扫地机器人","正在运行");
-        homeDeviceList.add(homeDevice9);
 
 
     }
