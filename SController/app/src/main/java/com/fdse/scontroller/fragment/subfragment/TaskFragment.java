d3d6bb26c800903bc18d36f30644db22c864e6f6
shenbiao <1105125966@qq.com>
2018-08-07 16:19:22 +0800

    把任务分成，我的，众包，请求（指定任务）

--- SController/app/src/main/java/com/fdse/scontroller/fragment/subfragment/TaskFragment.java
+++ SController/app/src/main/java/com/fdse/scontroller/fragment/subfragment/TaskFragment.java
@@ -1,55 +1,41 @@
 package com.fdse.scontroller.fragment.subfragment;
 
-import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import com.fdse.scontroller.LoginActivity;
-import com.fdse.scontroller.MainActivity;
 import com.fdse.scontroller.R;
-import com.fdse.scontroller.TasksWorkflowActivity;
-import com.fdse.scontroller.adapter.HomeDeviceAdapter;
-import com.fdse.scontroller.customview.HomeDevice;
-import com.melnykov.fab.FloatingActionButton;
+import com.fdse.scontroller.adapter.TaskViewPagerAdapter;
+import com.fdse.scontroller.fragment.tasksfragment.CrowdsourcingTasksFragment;
+import com.fdse.scontroller.fragment.tasksfragment.MyTasksFragment;
+import com.fdse.scontroller.fragment.tasksfragment.SpecifyTasksFragment;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Random;
 
 public class TaskFragment extends android.support.v4.app.Fragment {
     private View view;
-    private ListView listView;
-    private SwipeRefreshLayout swipeRefreshView;
-    private String[] listViewData;
-    private ArrayAdapter<String> adapter;
+    private TabLayout mTablayout;
+    private ViewPager mViewPager;
+    private List<String> mDatas;
+    private List<Fragment> fragments;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_sub_task, container, false);
 
-        //获取发布的任务
         initView();
         initData();
+        initFragments();
+        //todo 放在这里可以吗？getActivity()有效果吗？
         initAdapter();
-        //设置下拉刷新
-        setSwipeRefresh(view);
-
-        //设置voice附着在ListView，跟随ListView滚动滑入滑出
-        attachToListView();
 
         return view;
     }
@@ -60,90 +46,35 @@ public class TaskFragment extends android.support.v4.app.Fragment {
         super.onActivityCreated(savedInstanceState);
     }
 
-
     private void initView() {
-        listView = (ListView) view.findViewById(R.id.lv_tasks_publish);
+        mTablayout = (TabLayout) view.findViewById(R.id.tl_tasks);
+        mViewPager = (ViewPager)view. findViewById(R.id.vp_tasks);
     }
 
-    //添加bean类数据临时的先添加，再连数据库
     private void initData() {
-        listViewData = new String[20];
-        for (int i = 0; i < listViewData.length; i++) {
-            Date day=new Date();
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String time=df.format(day);
-            String sTask = "任务"+i+"             "+time;
-            listViewData[i]=sTask;
-            //item的点击事件，里面可以设置跳转并传值
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                    Toast.makeText(getActivity(), "第" + i + "行", Toast.LENGTH_LONG).show();
-                    //开始传值
-                    Intent intent=new Intent(getActivity(), TasksWorkflowActivity.class);
-                    Bundle bundle=new Bundle();
-                    bundle.putSerializable("key",i);
-                    intent.putExtras(bundle);
-                    //利用上下文开启跳转
-                    startActivity(intent);
-                }
-            });
-        }
+        mDatas = new ArrayList<>();
+        mDatas.add("我的");
+        mDatas.add("众包");
+        mDatas.add("请求");
+    }
+
+    private void initFragments() {
+        fragments = new ArrayList<Fragment>();
+        MyTasksFragment myTasksFragment = new MyTasksFragment();
+        fragments.add(myTasksFragment);
+        CrowdsourcingTasksFragment crowdsourcingTasksFragment = new CrowdsourcingTasksFragment();
+        fragments.add(crowdsourcingTasksFragment);
+        SpecifyTasksFragment specifyTasksFragment = new SpecifyTasksFragment();
+        fragments.add(specifyTasksFragment);
     }
 
     private void initAdapter() {
-        adapter = new ArrayAdapter<String>(
-                getActivity(), android.R.layout.simple_list_item_1, listViewData);
-        listView.setAdapter(adapter);
-    }
-
-    private void setSwipeRefresh(View view){
-
-        swipeRefreshView = (SwipeRefreshLayout) view.findViewById(R.id.swipe_tasks_publish);
-        // 设置颜色属性的时候一定要注意是引用了资源文件还是直接设置16进制的颜色，因为都是int值容易搞混
-        // 设置下拉进度的背景颜色，默认就是白色的
-        swipeRefreshView.setProgressBackgroundColorSchemeResource(android.R.color.white);
-        // 设置下拉进度的主题颜色
-        swipeRefreshView.setColorSchemeResources(R.color.colorAccent, R.color.colorPrimary, R.color.yellow);
-
-        // 下拉时触发SwipeRefreshLayout的下拉动画，动画完毕之后就会回调这个方法
-        swipeRefreshView.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-
-                // 开始刷新，设置当前为刷新状态
-                //swipeRefreshLayout.setRefreshing(true);
-
-                // 这里是主线程
-                // 一些比较耗时的操作，比如联网获取数据，需要放到子线程去执行
-                // TODO 获取数据
-                final Random random = new Random();
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-//                        HomeDevice homeDevice1 =new HomeDevice(R.drawable.home_xiaomidfb,"小米电饭煲","暂停运行");
-//                        homeDeviceList.add(homeDevice1);
-//                        homeDeviceAdapter.notifyDataSetChanged();
-
-                        Toast.makeText(getActivity(), "刷新成功", Toast.LENGTH_SHORT).show();
-
-                        // 加载完数据设置为不刷新状态，将下拉进度收起来
-                        swipeRefreshView.setRefreshing(false);
-                    }
-                }, 1200);
-
-                // System.out.println(Thread.currentThread().getName());
-
-                // 这个不能写在外边，不然会直接收起来
-                //swipeRefreshLayout.setRefreshing(false);
-            }
-        });
-    }
-
-    private void attachToListView(){
-        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab_voice);
-        fab.attachToListView(listView);
-        fab.setType(FloatingActionButton.TYPE_MINI);
+        mViewPager.setAdapter(new TaskViewPagerAdapter(getActivity().getSupportFragmentManager(), mDatas, fragments));
+        //todo    这个东西要不要加上
+         mTablayout.setupWithViewPager(mViewPager);
+        // 注意：我的布局文件中ViewPager和TabLayout 是嵌套的，所以不需要这一步
+        //          如果没有嵌套，则需要在ViewPager设置Adapter之后加上这一步
+        // 另外，只能是ViewPager嵌套Tablayout，反了会报错的，因为Tablayout中只能嵌套TabItem
     }
 
 }
